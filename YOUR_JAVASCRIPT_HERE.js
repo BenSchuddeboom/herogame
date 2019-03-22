e0b9e5dff6d7d1490d869e2d22be28fbc2560173
BenSchuddeboom <Benzel@live.nl>
2019-03-22 22:03:11 +0100

    All green first version

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,1 +1,35 @@
 // Write your JS here
+const hero = {
+    name: "Some hero",
+    heroic: true,
+    inventory: [],
+    health: 10,
+    weapon: {
+        type: "sword",
+        damage: 2,
+    }
+}
+
+const dagger = {
+    type: "dagger",
+    damage: 2,
+}
+
+function rest(hObj) {
+    if(hObj.health >= 10) {
+        alert("Your hero is already fully rested!")
+    } else {
+        hObj.health = 10;
+    }
+    return hObj;
+}
+
+function pickUpItem(hObj, weapon) {
+    hObj.inventory.push(weapon);
+}
+
+function equipWeapon(hObj) {
+    if(hObj.inventory[0]) {
+        hObj.weapon = hObj.inventory[0];
+    }
+}
