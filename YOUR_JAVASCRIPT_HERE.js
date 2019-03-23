bcf49c080ee6052dc1fc24fb0b6771780d08d4cb
BenSchuddeboom <Benzel@live.nl>
2019-03-23 10:45:54 +0100

    added displaystats functions

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -1,6 +1,6 @@
 // Write your JS here
 const hero = {
-    name: "Some hero",
+    name: "Bean",
     heroic: true,
     inventory: [],
     health: 10,
@@ -8,6 +8,16 @@ const hero = {
         type: "sword",
         damage: 2,
     }
+}
+
+const enemy = {
+    name: "Dragon",
+    inventory: [],
+    health: 20,
+    weapon: {
+        type: "breath",
+        damage: 3,
+    },
 }
 
 const dagger = {
@@ -34,6 +44,57 @@ function equipWeapon(hObj) {
     }
 }
 
-function displayStats {
+function displayHeroStats(hObj) {
+    const weaponType = hObj.weapon.type;
+    const weaponDamage = hObj.weapon.damage;
+    const name = hObj.name;
+    const health = hObj.health;
+
+    const stats = document.getElementById('heroStats');
+
+    const damage = document.createElement('li')
+    damage.innerText = "Damage: " + weaponDamage;
+    stats.appendChild(damage);
+
+    const weapon = document.createElement('li')
+    weapon.innerText = "Weapon: " + weaponType;
+    stats.appendChild(weapon);
+
+    const heroName = document.createElement('li')
+    heroName.innerText = "Name: " + name;
+    stats.appendChild(heroName);
+
+    const heroHealth = document.createElement('li')
+    heroHealth.innerText = "Health: " + health;
+    stats.appendChild(heroHealth);
 
 }
+
+function displayEnemyStats(eObj) {
+    const weaponType = eObj.weapon.type;
+    const weaponDamage = eObj.weapon.damage;
+    const name = eObj.name;
+    const health = eObj.health;
+
+    const stats = document.getElementById('enemyStats');
+
+    const damage = document.createElement('li')
+    damage.innerText = "Damage: " + weaponDamage;
+    stats.appendChild(damage);
+
+    const weapon = document.createElement('li')
+    weapon.innerText = "Weapon: " + weaponType;
+    stats.appendChild(weapon);
+
+    const heroName = document.createElement('li')
+    heroName.innerText = "Name: " + name;
+    stats.appendChild(heroName);
+
+    const heroHealth = document.createElement('li')
+    heroHealth.innerText = "Health: " + health;
+    stats.appendChild(heroHealth);
+
+}
+
+displayEnemyStats(enemy);
+displayHeroStats(hero)
